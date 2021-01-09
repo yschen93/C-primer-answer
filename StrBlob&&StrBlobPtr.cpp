59fd2960b8899a3cfef6e243fd038ee01c40466f
Martin <40158280+yschen93@users.noreply.github.com>
2021-01-09 17:12:23 +0800

    update class StrBlobPtr

--- StrBlob&&StrBlobPtr.cpp
+++ StrBlob&&StrBlobPtr.cpp
@@ -5,9 +5,54 @@
 
 using namespace std;
 
+//================StrBlobPtr================
+
+class StrBlob;
+
+class StrBlobPtr
+{
+public:
+	StrBlobPtr() : cur(0) {}
+	StrBlobPtr(StrBlob &a, size_t sz = 0);
+	string& deref();
+	StrBlobPtr& incur();
+private:
+	weak_ptr<vector<string> > wkptr;
+	size_t cur;
+	shared_ptr<vector<string> > check(size_t i, const string& msg) const;
+};
+
+string& StrBlobPtr::deref()
+{
+	auto ptr = check(cur, "dereference pass end");
+	return (*ptr)[cur];
+}
+
+shared_ptr<vector<string> > StrBlobPtr::check(size_t i, const string& msg) const
+{
+	auto ret = wkptr.lock();
+	if (!ret)
+		throw runtime_error("unbound StrBlobPtr");
+	if (i >= ret->size())
+	{
+		throw out_of_range(msg);
+	}
+	return ret;
+}
+
+StrBlobPtr& StrBlobPtr::incur()
+{
+	check(cur, "dereference pass end");
+	++cur;
+	return *this;
+}
+
+//================StrBlob================
+
 class StrBlob
 {
 public:
+	friend StrBlobPtr;
 	using size_type = vector<string>::size_type;
 	StrBlob() : data(make_shared<vector<string> >()) {}
 	explicit StrBlob(initializer_list<string> il) : data(make_shared<vector<string> >(il)) {}
@@ -19,6 +64,9 @@ public:
 	const string& front() const;
 	string& back();
 	const string& back() const;
+
+	StrBlobPtr begin();
+	StrBlobPtr end();
 private:
 	shared_ptr<vector<string> > data;
 	void check(size_type i, const string& msg) const;
@@ -60,9 +108,26 @@ void StrBlob::pop_back()
 	data->back();
  }
 
+StrBlobPtr StrBlob::begin()
+{
+	return StrBlobPtr(*this, 0);
+}
+StrBlobPtr StrBlob::end()
+{
+	return StrBlobPtr(*this, size());
+}
+
+//Why here? ues StrBlob definition
+StrBlobPtr::StrBlobPtr(StrBlob &a, size_t sz)
+	: wkptr(a.data), cur(sz)
+{
+
+}
+
 int main()
 {
-	StrBlob temp({ "I", "am", "c++" });
+	StrBlob temp({ "I", "am", "C++" });
 	system("pause");
 	return 0;
 }
+
